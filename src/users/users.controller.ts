31d601c9cb6305bd2c1fdd531ab9962dc43ce76f
VladyslavVagin <vladtwince@gmail.com>
2024-08-22 11:17:57 +0200

    update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,14 +10,20 @@ import {
   Query,
   Body,
   Headers,
+  ParseIntPipe,
+  DefaultValuePipe,
 } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-  @Get('/:id/:optional?')
-  public getUsers(@Param('id') id: any, @Query('limit') limit: any) {
-    console.log(id);
+  @Get('/:id?')
+  public getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
     console.log(limit);
+    console.log(page);
     return 'You send a GET request to /users endpoint';
   }
 
